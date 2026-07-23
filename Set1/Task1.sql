6a90ecea3d59f3a7677d5e24850b17de9ba4dc71
erohith-sys <e.rohith@btech.christuniversity.in>
2026-07-23 19:44:37 +0530

    Create Task1.sql

--- Set1/Task1.sql
+++ Set1/Task1.sql
@@ -1,1 +1,14 @@
+mysql> CREATE DATABASE ChristUniversity_db;
+Query OK, 1 row affected (0.01 sec)
 
+mysql> USE ChristUniversity_db;
+Database changed
+mysql> CREATE TABLE students (
+    ->     student_id INT PRIMARY KEY,
+    ->     name VARCHAR(50),
+    ->     course VARCHAR(50),
+    ->     marks INT,
+    ->     city VARCHAR(50),
+    ->     email VARCHAR(100)
+    -> );
+Query OK, 0 rows affected (0.03 sec)
